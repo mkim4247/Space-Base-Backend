83354e9f2c8b60997ad112c637f9fdadddf4e158
Michael Kim <m.kim4247@gmail.com>
2019-01-18 16:43:21 -0500

    cleaned up

--- db/migrate/20190108193712_create_towers.rb
+++ db/migrate/20190108193712_create_towers.rb
@@ -3,7 +3,7 @@ class CreateTowers < ActiveRecord::Migration[5.2]
     create_table :towers do |t|
       t.string :name
       t.integer :happiness, default: 50
-      t.integer :funds, default: 1000
+      t.integer :resources, default: 1000
       t.integer :population, default: 1
       t.integer :defense, default: 25
       t.integer :user_id
